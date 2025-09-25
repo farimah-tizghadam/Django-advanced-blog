f0c8104844f076686514b5b1f02b70392f8553d7
Farimah <108592778+farimah-tizghadam@users.noreply.github.com>
2025-09-25 17:27:22 +0330

    add crud to post model

---
+++ core/blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.views.generic.base import RedirectView
+
+app_name= 'blog'
+
+urlpatterns = [
+    # path('cbv-index', TemplateView.as_view(template_name='index.html', extra_context={'name':'ali'})),
+    path('cbv-index', views.IndexView.as_view(), name="index"),
+    # path('go-to-index', RedirectView.as_view(pattern_name='blog:index'),name='redirect-to-index'),
+    # path('go-to-maktab/<int:pk>/', views.RedirectToMaktab.as_view(),name='redirect-to-maktabkhooneh'),
+    path('post/', views.PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/create/', views.PostCreateView.as_view(),name='post-create'),
+    path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post-edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,86 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import ListView, DetailView, FormView, CreateView, UpdateView, DeleteView
+from django.shortcuts import get_list_or_404
+from .models import Post
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    """
+    class base view to show index page
+    """
+    template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'farimah'
+        return context
+    
+
+"""class base view
+
+"""
+
+"""class RedirectToMaktab(RedirectView):
+
+    url = 'https://maktabkhooneh.com'
+
+
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_list_or_404(Post, id=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)"""
+
+
+class PostListView(PermissionRequiredMixin,LoginRequiredMixin,ListView):
+    permission_required = 'blog:post_view'
+    # model = Post
+    queryset = Post.objects.filter(status=True)
+    paginate_by = 2
+    ordering = '-id'
+
+    # def get_queryset(self):
+    #     posts = Post.objects.all()
+    #     return posts
+
+    context_object_name = 'posts'
+ 
+
+class PostDetailView(LoginRequiredMixin,DetailView):
+    model = Post
+    context_object_name = 'post'
+
+
+"""
+class PostCreateView(FormView):
+    template_name = "blog/contact.html"
+    form_class = PostForm
+    success_url = "/blog/post/"
+
+    def form_valid(self, form):
+        form.save()
+        return super(PostCreateView, self).form_valid(form)
+
+"""
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    # fields = ['author', 'status', 'title', 'content', 'category', 'published_date']
+    form_class = PostForm
+    success_url = "/blog/post/"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostEditView(LoginRequiredMixin,UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = "/blog/post/"
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = "/blog/post/"
+
