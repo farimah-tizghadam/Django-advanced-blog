ab273ad09f10df8a5f8ea40b2792f89b242d9ba1
Farimah <108592778+farimah-tizghadam@users.noreply.github.com>
2025-10-19 13:47:28 +0330

    use redis and celery for running tasks

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import send_email 
+from . import views 
 
 app_name = "accounts"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("api/v1/", include("accounts.api.v1.urls")),
-    path('send-email/', send_email, name='send-email')
+    path('send-email/', views.send_email, name='send-email'),
+    path('test/', views.test, name='test'),
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,9 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .tasks import sending_email
-
-
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
+import requests
 
 
 def send_email(request):
     sending_email.delay()
     return HttpResponse("<h1>Done Sending</h1>")
+
+# without using decorator
+"""def test(request):
+    if cache.get("test_delay_api") is None:
+        response = requests.get('https://790fa2f6-ba9d-40be-8c7a-27479edfaa33.mock.pstmn.io/test/delay/5')
+        cache.set('test_delay_api', response.json())
+    return JsonResponse(cache.get('test_delay_api'))"""
+
+#using decorator
+@cache_page(60)
+def test(request):
+    response = requests.get('https://790fa2f6-ba9d-40be-8c7a-27479edfaa33.mock.pstmn.io/test/delay/5')
+    return JsonResponse(response.json())
